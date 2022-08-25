c0f5a7601992cc68a6df6c71494c10ae51d2f225
Harshit Pachori <harshitpachori345@gmail.com>
2022-08-25 12:23:55 +0530

    day2

--- FindDuplicate.java
+++ FindDuplicate.java
@@ -1,6 +1,37 @@
+import java.util.*;
+
 public class FindDuplicate {
 
-    public static int findDuplicateNumber(int nums[]) {
+    // Naive Approach BruteForce
+    // Time comp -> O(nlogn) space comp -> O(1)
+    public static int findDuplicateNumberByBruteForceMethod(int nums[]) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length + 1; i++) {
+            if (nums[i - 1] == nums[i]) {
+                return nums[i];
+            }
+        }
+        return -1;
+
+    }
+
+    // Hashing optimized solution
+    // Time comp -> O(n) space comp -> O(n)
+    public static int findDuplicateNumberByHashingMethod(int nums[]) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return nums[i];
+            }
+            map.put(nums[i], i);
+        }
+        return -1;
+
+    }
+
+    // Linkedlist Cycle Method optimized solution
+    // Time comp -> O(n) Space comp -> O(1)
+    public static int findDuplicateNumberByLinkedlistCycleMethod(int nums[]) {
         int slow = nums[0];
         int fast = nums[0];
         do {
@@ -17,8 +48,13 @@ public class FindDuplicate {
 
     public static void main(String[] args) {
         int arr[] = { 2, 5, 9, 6, 9, 3, 8, 9, 7, 1 };
-        int result = findDuplicateNumber(arr);
-        System.out.println("Duplicate number is : " + result);
+        int result1 = findDuplicateNumberByBruteForceMethod(arr);
+        int result2 = findDuplicateNumberByHashingMethod(arr);
+        int result3 = findDuplicateNumberByLinkedlistCycleMethod(arr);
+
+        System.out.println("Duplicate number by BruteForce Method is : " + result1);
+        System.out.println("Duplicate number by Hashing Method is : " + result2);
+        System.out.println("Duplicate number by LinkedList Cycle Method is : " + result3);
     }
 
 }
---
+++ SortArrayOf012.java
@@ -0,0 +1,9 @@
+public class SortArrayOf012 {
+    public static int[] sortMethod( int []nums){
+        int ;
+        return nums;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
