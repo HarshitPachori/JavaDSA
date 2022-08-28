f36d8ea7a5a476bbea685fd6c28f5da2ec545362
Harshit Pachori <harshitpachori345@gmail.com>
2022-08-28 19:00:46 +0530

    day 2

---
+++ ReverseArray.java
@@ -0,0 +1,29 @@
+public class ReverseArray {
+    public static void reverseArray(int arr[], int n) {
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void printArray(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(); // for line change
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 2, 1, 0 };
+        printArray(arr, 6);  // before reverse 
+        reverseArray(arr, 6);
+        printArray(arr, 6); // after reverse 
+
+    }
+}
