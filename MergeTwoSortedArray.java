cc6017759dd25ff3599b7c6e3d7f418faf1bc855
Harshit Pachori <harshitpachori345@gmail.com>
2022-08-31 11:36:58 +0530

    day3

--- MergeTwoSortedArray.java
+++ MergeTwoSortedArray.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class MergeTwoSortedArray {
-    public static void merge(int arr1[], int arr2[], int m, int n, int[] arr3) {
+    public static void merge(int arr1[], int arr2[], int m, int n) {
         int i = 0, j = 0, k = 0;
+        int temp[] = new int[m + n];
 
         while (i < m && j < n) {
             if (arr1[i] < arr2[j]) {
-                arr3[k++] = arr1[i++];
+                temp[k++] = arr1[i++];
             } else {
-                arr3[k++] = arr2[j++];
+                temp[k++] = arr2[j++];
             }
         }
         while (i < m) {
-            arr3[k++] = arr1[i++];
+            temp[k++] = arr1[i++];
 
         }
         while (j < n) {
-            arr3[k++] = arr2[j++];
+            temp[k++] = arr2[j++];
+        }
+        for (int a = 0; a < m + n; a++) {
+            arr1[a] = temp[a];
         }
 
     }
@@ -35,9 +39,9 @@ public class MergeTwoSortedArray {
         System.out.println("Enter the size of both arr1 and arr2: (hint: arr1>=5 and arr2>=5) ");
         m = sc.nextInt();
         n = sc.nextInt();
-        int arr1[] = new int[m];
+        int arr1[] = new int[m + n];
         int arr2[] = new int[n];
-        int arr3[] = new int[m + n];
+
         arr1[0] = 2;
         arr1[1] = 4;
         arr1[2] = 6;
@@ -49,8 +53,8 @@ public class MergeTwoSortedArray {
         arr2[3] = 7;
         arr2[4] = 9;
         // printArray(arr3, 9);
-        merge(arr1, arr2, m, n, arr3);
-        printArray(arr3, m + n);
+        merge(arr1, arr2, m, n);
+        printArray(arr1, m + n);
         sc.close();
 
     }
