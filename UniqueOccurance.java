15184da7af49c1a7e5ffd8808ed4fa6a8e15a6ee
Harshit Pachori <harshitpachori345@gmail.com>
2022-09-09 19:43:46 +0530

    day 7

---
+++ UniqueOccurance.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class UniqueOccurance {
+    public static boolean occurances(int arr[]) {
+        Arrays.sort(arr);
+        ArrayList<Integer> list = new ArrayList<>();
+        int count = 0;
+        int temp = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == temp)
+                count++;
+            else {
+                if (list.contains(count))
+                    return false;
+                else {
+                    list.add(count);
+                    count = 1;
+                    temp = arr[i];
+                }
+            }
+        }
+        if (list.contains(count))
+            return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 2, 1, 1, 3 };
+        boolean result = occurances(arr);
+        System.out.println(result);
+    }
+}
